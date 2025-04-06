d64438723c5680d67a5d5b449f6941b33fa7e1ce
Densodim <25denso@gmail.com>
2025-04-06 23:51:17 +0300

    first commit

---
+++ first_letter.py
@@ -0,0 +1,3 @@
+response = input("Tell me your password: ")
+first_letter = response[0].upper()
+print("The first letter you entered was:" + first_letter)
---
+++ start.py
@@ -0,0 +1,7 @@
+response = input("Please write text")
+lower_response = response.lower()
+count_response = len(response)
+
+print("This text from input..." + response)
+print("This text from input lower text..." + lower_response)
+print(count_response)

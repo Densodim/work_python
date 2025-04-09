7dd9a38db3d733d5e3a09f801e6892839cf4b28c
Densodim <25denso@gmail.com>
2025-04-09 14:43:37 +0300

    add exponent

---
+++ exponent.py
@@ -0,0 +1,6 @@
+num1 = input('Enter a base:')
+num2 = input('Enter an exponent:')
+num1_int = float(num1);
+num2_int = float(num2);
+exponent = num1_int ** num2_int
+print(f'{num1} to the power of {num2} = {exponent}')
